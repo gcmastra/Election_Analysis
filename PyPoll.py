1d2b2c5653c5c54f553ada07b8960846efea82e0
gcmastra <86205000+gcmastra@users.noreply.github.com>
2021-07-11 21:46:20 -0400

    Add files via upload
    This version is how the code looks at the end of unity 3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -10,33 +10,45 @@ file_to_load = os.path.join("resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# Initialize a total vote counter.
+total_votes = 0
 
-    # Write some data to the file.
-    txt_file.write("Counties In The Election\n")
-    txt_file.write("------------------------- \n")
-    # Write three counties to the file.
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+# Candidate Options
+candidate_options = []
 
-# Close the file
-txt_file.close()
-
-# Open the election results and read the file
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-#   # To do: perform analysis.
-#   # All the GOOD STUFF happens here
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Read and Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
 
     # Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row)
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+
+# Leave some space in the print out
+print("\n")
+
+# Print the candidate list.
+print("List of Candidates")
+print(candidate_options)
+
+print("\n\n")
+
+print("Total_votes = ", total_votes)
+# Leave some space in the print out
+print("\n")
 
 # # Close the file.
 election_data.close()
